ff0751e17b3700d2a04f4559390e7983b10ad0f7
Eric Ferguson <turtle0888@gmail.com>
2019-12-17 15:55:11 -0600

    Add login form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <Router>
+    <div className='App'>
+      <Switch>
+        <Route path='login' component={LoginForm} />
+        <Route component={LoginForm} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
