283661de2267b929951ae212f048dbba2696c4c3
Eric Ferguson <turtle0888@gmail.com>
2019-12-17 17:26:10 -0600

    Add friends list

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 
 import './App.css';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
     <Router>
     <div className='App'>
       <Switch>
+        <PrivateRoute path='/protected' component={FriendsList} />
         <Route path='login' component={LoginForm} />
         <Route component={LoginForm} />
       </Switch>
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+
+class FriendsList extends React.Component {
+  state = {
+    friends: []
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    axiosWithAuth()
+    .get('/friends')
+    .then(res => {
+      this.setState({friends: res.data});
+    })
+  }
+
+
+  render(){
+    return (
+      <div>
+        <div>
+          {this.state.friends.map((friend, id) => (
+            <div key={friend.id}>
+              <h1>{friend.name}</h1>
+              <h2>Age: {friend.age}</h2>
+              <h2>Email: {friend.email}</h2>
+
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+  };
+
+
+export default FriendsList;
